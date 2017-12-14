bc78259b44b3dc309c2dfe1265d8d58ef805adb9
sebastien pattyn <sebastien.pattyn@gmail.com>
2017-12-14 16:30:00 +0100

    added metrics + first steps for clustering

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Overview
 
 ArangoDB is a native multi-model, open-source database with flexible
-data models for documents, graphs, and key-values. This charm installs and 
+data models for documents, graphs, and key-values. This charm installs and
 configures [ArangoDB](https://arangodb.com/).
 
 # Usage
@@ -9,13 +9,9 @@ configures [ArangoDB](https://arangodb.com/).
 Deploy the ArangoDB charm with the following:
 
 ```bash
-juju deploy cs:~tengu-team/arangodb-0
+juju deploy cs:~tengu-team/arangodb-1
 ```
 The ArangoDB UI is then available at `http://x.x.x.x:8592`. If the root password is not provided in the `config.yaml`, then the root password will be auto-generated and will be shown in the status of the charm.
-
-# Limitations
-- At this moment ArangoDB can only run in stand-alone mode. Clustering ArangoDB with this charm is still in progress.
-- The charm currently provides a http interface but will provide a custom interface which will also provide the root password.
 
 # Contact Information
 
@@ -23,3 +19,4 @@ The ArangoDB UI is then available at `http://x.x.x.x:8592`. If the root password
 ## Authors
 
  - Dixan Peña Peña <dixan.pena@tengu.io>
+ - Sébastien Pattyn <sebastien.pattyn@tengu.io>
--- reactive/layer_arangodb.py
+++ reactive/layer_arangodb.py
@@ -13,75 +13,151 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# pylint: disable=c0111,c0103,c0301
+# pylint: disable=c0111,c0103,c0301,e0401
 import os
 import subprocess
+import random
 from base64 import b64encode
+from charms import leadership
+from charms.reactive import when, when_not, set_state
 from charmhelpers.core import unitdata
 from charmhelpers.core.templating import render
-from charmhelpers.core.host import service_restart
-from charms.reactive import when, when_not, set_state
-from charmhelpers.core.hookenv import status_set, open_port, close_port, config, unit_public_ip
+from charmhelpers.core.host import service_restart, service_stop
+from charmhelpers.core.hookenv import status_set, open_port, close_port, config, unit_public_ip, leader_get, leader_set, unit_private_ip
+
+
 
 kv = unitdata.kv()
-
+################################################################################
+# Install
+################################################################################
 @when('apt.installed.arangodb3')
-@when_not('arangodb.configured')
+@when_not('arangodb.installed')
 def configure_arangodb():
     status_set('maintenance', 'configuring ArangoDB')
-    conf = config()
-    port = conf['port']
-    authn = conf['authentication']
-    password =conf['root_password']
-    if password == "":
-        password = b64encode(os.urandom(18)).decode('utf-8')
-    kv.set('password', password)
-    render(source='arangod.conf',
-           target='/etc/arangodb3/arangod.conf',
-           context={
-               'port': str(port),
-               'authentication': str(authn).lower()
-           })
-    require = "require('@arangodb/users').update('root', '{}', true)".format(password)
-    subprocess.check_call(['arangosh', '--server.username', 'root', '--server.password', '', '--javascript.execute-string', require])
-    open_port(port)
-    set_state('arangodb.configured')
-
-@when('arangodb.configured')
-@when_not('arangodb.running')
-def start_arangodb():
-    service_restart("arangodb3")
+    install_standalone()
     status_set('active', 'ArangoDB running with root password {}'.format(kv.get('password')))
-    set_state('arangodb.running')
+    set_state('arangodb.installed')
 
-@when('arangodb.running', 'http.available')
-def configure_http(http):
+@when('arangodb.installed', 'db.available')
+def configure_http(db):
     conf = config()
-    http.configure(conf['port'])
+    db.configure(conf['port'], kv.get('username'), kv.get('password'))
 
-@when('arangodb.running', 'config.changed')
+
+@when('arangodb.installed', 'config.changed')
 def change_configuration():
     status_set('maintenance', 'configuring ArangoDB')
     conf = config()
-    port = conf['port']
-    old_port = conf.previous('port')
-    authn = conf['authentication']
+    change_config(conf)
+    service_restart('arangodb')
+    status_set('active', 'ArangoDB running with root password {}'.format(kv.get('password')))
+
+################################################################################
+# Leadership
+################################################################################
+@when('leadership.is_leader')
+@when_not('secrets.configured')
+def set_secrets():
+    password = leader_get().get('password', kv.get('password'))
+    leader_set({'password': password , 'first_ip': unit_private_ip()})
+    kv.set('password', password)
+    set_state('secrets.configured')
+
+
+@when('arangodb.installed')
+@when_not('leadership.is_leader')
+def set_secrets_local():
+    kv.set('password', leader_get()['password'])
+    status_set('active', 'ArangoDB running with root password {}'.format(kv.get('password')))
+################################################################################
+# clustering
+################################################################################
+@when('cluster.joined')
+def configure_cluster(cluster):
+    units = cluster.get_peer_addresses()
+    install_cluster(units, True)
+
+
+@when('cluster.depaterd')
+def change_cluster(cluster):
+    units = cluster.get_peer_addresses()
+
+    install_cluster(units)
+################################################################################
+# Helper Functions
+################################################################################
+def change_config(conf):
     if conf.changed('port') or conf.changed('authentication'):
+        old_port = conf.previous('port')
         render(source='arangod.conf',
                target='/etc/arangodb3/arangod.conf',
                context={
-                   'port': str(port),
-                   'authentication': str(authn).lower()
+                   'port': str(conf['port']),
+                   'authentication': str(conf['authentication']).lower()
                })
         if old_port is not None:
             close_port(old_port)
-        open_port(port)
+        open_port(conf['port'])
     if conf['root_password'] != kv.get('password') and conf['root_password'] != "":
         password = conf['root_password']
         old_password = kv.get('password')
         kv.set('password', password)
-        TCP = 'tcp://' + unit_public_ip() + ':' + str(port)
+        TCP = 'tcp://' + unit_public_ip() + ':' + str(conf['port'])
         require = "require('@arangodb/users').update('root', '{}', true)".format(password)
         subprocess.check_call(['arangosh', '--server.endpoint', TCP, '--server.username', 'root', '--server.password', old_password, '--javascript.execute-string', require])
-    service_restart("arangodb3")
-    status_set('active', 'ArangoDB running with root password {}'.format(kv.get('password')))
+
+
+def install_cluster(units, expanded=False):
+    kv.set('cluster', units)
+    if len(units) == 1:
+        install_standalone()
+    elif expanded:
+        if len(units) == 2:
+            for unit in units:
+                if unit == leader_get('first_ip'):
+                    install_standalone()
+                else:
+                    status_set('blocked', 'Arangodb needs at least three units to run in clustering mode!')
+        else:
+            install_clustered(units)
+    else:
+        if len(units) == 2:
+            status_set('blocked', 'Arangodb needs at least three units to run in clustering mode!')
+            subprocess.check_call(['arangodb', 'stop'])
+        else:
+            upgrade_cluster(units)
+
+
+def install_standalone():
+    conf = config()
+    password = conf['root_password']
+    kv.set('username', 'root')
+    render(source='arangod.conf',
+           target='/etc/arangodb3/arangod.conf',
+           context={
+               'port': str(conf['port']),
+               'authentication': str(conf['authentication']).lower()
+           })
+    if password == "" and not leader_get()['password']:
+        password = b64encode(os.urandom(18)).decode('utf-8')
+        require = "require('@arangodb/users').update('root', '{}', true)".format(password)
+        subprocess.check_call(['arangosh', '--server.username', 'root', '--server.password', '', '--javascript.execute-string', require])
+    else:
+        password = leader_get()['password']
+    kv.set('password', password)
+    open_port(conf['port'])
+    service_restart('arangodb')
+
+
+def install_clustered(units):
+    service_stop('arangodb')
+    for unit in units:
+        if unit == leader_get('master_ip'):
+            subprocess.check_call(['arangodb', 'start'])
+        else:
+            subprocess.check_call(['arangodb', 'start', '--starter.join', leader_get('master_ip')])
+
+def upgrade_cluster(units):
+    rest = units.remove(unit_private_ip())
+    subprocess.check_call(['arangodb', 'start', '--starter.join', random.choice(rest)])
